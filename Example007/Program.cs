4c414678027f387ab882f3dd0fd11ad948d42d84
Konstantin <malyshevkd@gmail.com>
2023-01-09 21:17:26 +0300

    add example 007

---
+++ Example007/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+Clear();
+
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+int first = number/100;
+int last = number%10;
+
+Write(first);
+Write(last);
+
