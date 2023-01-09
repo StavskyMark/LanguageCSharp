800b2022d251ed4d80f572b79301e86bac723b6a
Konstantin <malyshevkd@gmail.com>
2023-01-09 22:18:22 +0300

    Example 009

---
+++ Example009/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите число a: ");
+int a = int.Parse(ReadLine());
+Write("Введите число b: ");
+int b = int.Parse(ReadLine());
+
+WriteLine(a == b? "=": a > b? ">" : "<");
