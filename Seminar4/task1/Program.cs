f8f16a5e59f8318662602921c0f33466fb1fbc0e
Konstantin <malyshevkd@gmail.com>
2023-01-12 21:13:28 +0300

    add solut task 1

---
+++ Seminar4/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+
+
+// Write("Input number1: ");
+// int number1 = int.Parse(ReadLine()!);
+
+// int sum1=GetSum(number1);
+// WriteLine($"Sum number 1..A = {sum1}");
+// WriteLine(number1);
+
+// int GetSum(int A)
+// {
+//     int result = 0;
+//     while(A > 0)
+//     {
+//         result += A;
+//         A--;
+//     }
+
+//     return result;
+// }
+
+WriteLine("Введите число: ");
+int number = Convert.ToInt32 (ReadLine()!);
+int count = Numbers(number);
+
+
+
+int Numbers(int A)
+{
+    int result = 0;
+
+    while(A > 0)
+    {
+        A = A / 10;
+        result ++;
+    }
+
+    
+}
