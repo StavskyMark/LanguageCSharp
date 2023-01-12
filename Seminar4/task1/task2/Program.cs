9572b85aa2546f1cdc6ba9a95bad1948373cc7bb
Konstantin <malyshevkd@gmail.com>
2023-01-12 21:14:24 +0300

    add solut task 2

---
+++ Seminar4/task1/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число:" );
+int Number = int.Parse(ReadLine()!);
+
+int mult = Getmult(Number);
+WriteLine($"Произведение от 1 до N равно {mult}");
+
+
+int Getmult(int A)
+{
+    int result = 1;
+
+    while (A>0)
+    {
+        result*=A;
+        --A;
+    }
+
+    return result;
+}
+
