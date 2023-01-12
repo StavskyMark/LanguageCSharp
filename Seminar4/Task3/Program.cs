2ca57216b607ebe4c18b9fa846775b1482de2795
Konstantin <malyshevkd@gmail.com>
2023-01-12 22:11:32 +0300

    add solut task 3

---
+++ Seminar4/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+Clear();
+
+int [] arr = GetArray();
+
+GetArray();
+PrintArray(arr);
+
+
+void FullArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+
+int[] GetArray ()
+{
+    int [] arr = new int [8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i]= new Random().Next(2);
+    }
+    return arr;
+}
+
+void PrintArray (int [] arr)
+{
+    for (int i =0; 1 < arr.Length; i++)
+    {
+        WriteLine($"{arr[i]} ");
+    }
+}
