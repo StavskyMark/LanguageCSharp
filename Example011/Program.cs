ef2525b7a1a2a22720ada0a260b242efdb337d42
Konstantin <malyshevkd@gmail.com>
2023-01-12 18:24:45 +0300

    add Example 011

---
+++ Example011/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите чисто a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число b: ");
+int b = Convert.ToInt32(ReadLine());
+
+//if (a * a == b || b * b == a)
+
+WriteLine(a * a == b? "b является квадратом a":b * b == a? "a является квадратом b":"Числа не являются квадратами друг друга");
+
