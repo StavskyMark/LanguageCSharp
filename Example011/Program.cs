6f12e47139a4e55a9afabb4af2c31e5ae83a21f1
Konstantin <malyshevkd@gmail.com>
2023-01-12 19:04:46 +0300

    add solurion task 2

--- Example011/Program.cs
+++ Example011/Program.cs
@@ -5,8 +5,8 @@ WriteLine("Введите чисто a: ");
 int a = Convert.ToInt32(ReadLine());
 WriteLine("Введите число b: ");
 int b = Convert.ToInt32(ReadLine());
+int total = a + b - 1;
 
-//if (a * a == b || b * b == a)
+//WriteLine(a * a == b? "b является квадратом a":b * b == a? "a является квадратом b":"Числа не являются квадратами друг друга");
 
-WriteLine(a * a == b? "b является квадратом a":b * b == a? "a является квадратом b":"Числа не являются квадратами друг друга");
-
+WriteLine("Всего было банок " + total );
