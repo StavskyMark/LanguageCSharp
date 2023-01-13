3ef105d2cb942b55127c79e7ead9cb00317ddd7d
Konstantin <malyshevkd@gmail.com>
2023-01-13 15:49:28 +0300

    add solution task days of the weak

---
+++ Example013/Program.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите номер дня недели: ");
+int i = Convert.ToInt32(ReadLine()!);
+
+switch(i)
+ {
+     case 1:
+         {
+         WriteLine("Понеделдьник");
+         break;
+         }
+     case 2:
+         {
+         WriteLine("Вторник");
+         break;
+         }
+
+     case 3:
+         {
+         WriteLine("Среда");
+         break;
+         }
+     case 4:
+         {
+         WriteLine("Четверг");
+         break;
+         }
+    case 5:
+        {
+            WriteLine("Пятница");
+            break;
+        }
+    case 6:
+        {
+            WriteLine("Суббота");
+            break;
+        }
+    case 7:
+        {
+            WriteLine("Воскресенье");
+            break;
+        }
+         default:
+         {
+             WriteLine("Введите корректный номер дня недели!");
+             break;
+         }
+ }
---
+++ HomeWork002/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Write("Введите число a: ");
+int a = int.Parse(ReadLine());
+Write("Введите число b: ");
+int b = int.Parse(ReadLine());
+Write("Введите число c: ");
+int c = int.Parse(ReadLine());
+
+
+if 
+{   
+    if (a > b)
+    Write(a);
+}
+else
+{
+    Write(b);
+}
