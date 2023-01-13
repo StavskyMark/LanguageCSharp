431cb6af77dbb48941c58ab21162108ce27dadfe
Konstantin <malyshevkd@gmail.com>
2023-01-13 18:11:11 +0300

    add solution example 014

---
+++ Example014/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите кол-во сделаных деталей за смену: ");
+int n = Convert.ToInt32(ReadLine()!);
+int result = n%6;
+
+if( result == 0) WriteLine($"Младшие токари сделали по {n/6} детали, старший - {n/6*4} деталей");
+else WriteLine("Старший токарь ошибся");
+
