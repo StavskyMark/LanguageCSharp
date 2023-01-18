f2ec7fb3d993c21ca1eaaf3bc1f382d0879ceca4
Konstantin <malyshevkd@gmail.com>
2023-01-18 17:04:47 +0300

    add solution task 27

---
+++ HomeWork/Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+
+ Write("Введите число: ");
+ int i = Convert.ToInt32(ReadLine()!);
+
+ int Sum = GetSum(i);
+ WriteLine($"Сумма чисел = {Sum}");
+
+
+
+ int GetSum(int A)
+ {
+     int result = 0;
+     while (A > 0)
+     {
+        int d = i % 10;
+         result += d;
+         i = i/10;
+         A--;
+     }
+     return result;
+ }
--- Seminar4/task1/Program.cs
+++ Seminar4/task1/Program.cs
@@ -1,40 +1,40 @@
 ﻿using static System.Console;
+Clear();
+
+ Write("Input number1: ");
+ int number1 = int.Parse(ReadLine()!);
+
+int sum1=GetSum(number1);
+ WriteLine($"Sum number 1..A = {sum1}");
+ WriteLine(number1);
+
+ int GetSum(int A)
+ {
+     int result = 0;
+     while(A > 0)
+     {
+         result += A;
+         A--;
+     }
+
+     return result;
+ }
+
+// WriteLine("Введите число: ");
+// int number = Convert.ToInt32 (ReadLine()!);
+// int count = Numbers(number);
 
 
-// Write("Input number1: ");
-// int number1 = int.Parse(ReadLine()!);
 
-// int sum1=GetSum(number1);
-// WriteLine($"Sum number 1..A = {sum1}");
-// WriteLine(number1);
-
-// int GetSum(int A)
+// int Numbers(int A)
 // {
 //     int result = 0;
+
 //     while(A > 0)
 //     {
-//         result += A;
-//         A--;
+//         A = A / 10;
+//         result ++;
 //     }
 
-//     return result;
-// }
-
-WriteLine("Введите число: ");
-int number = Convert.ToInt32 (ReadLine()!);
-int count = Numbers(number);
-
-
-
-int Numbers(int A)
-{
-    int result = 0;
-
-    while(A > 0)
-    {
-        A = A / 10;
-        result ++;
-    }
-
     
-}
+// }
