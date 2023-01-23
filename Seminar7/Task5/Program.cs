90d1a89f365bcaa2291cc585c1ae0763136cc8ae
Konstantin <malyshevkd@gmail.com>
2023-01-23 22:03:17 +0300

    add seminar7 task 5

---
+++ Seminar7/Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите адрес клетки:: ");
+int[] addres = Array.ConvertAll(ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+WriteLine(isBlack(addres) ? "Черная" : "Белая");
+
+bool isBlack (int[] addres){
+    if(addres[0] %2 == 0) return addres[1] % 2 == 0;
+    else return addres[1] % 2 != 0;
+}
