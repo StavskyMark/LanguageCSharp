03584c6e36d04d89d14a17553c2bce524ec7c18e
Konstantin <malyshevkd@gmail.com>
2023-01-26 17:53:43 +0300

    add solution task 41

---
+++ HomeWork/Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите числа через пробел: ");
+string[] parameters = Readline()!.Split(" ",stringSplitOptions.RemoveEmptyEntries);
+
+CountNull();
+
+int CountNull ()
+{
+    int count = 0;
+    for (int i = 0; i < parameters.Length; i++)
+    {
+        if (parameters[i] > 0);
+        {
+            count ++;
+        }  
+        WriteLine($"Кол-во элементов > 0: {count}");
+    }
+}
+
