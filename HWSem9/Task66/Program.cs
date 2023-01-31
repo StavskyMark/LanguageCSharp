1088c111daafdc559beda7bccc30996fa844140e
Konstantin <malyshevkd@gmail.com>
2023-01-31 15:27:04 +0300

    Solution Task 66

---
+++ HWSem9/Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+using static System.Console;
+Clear();
+
+
+Write("Введите начальное число M: ");
+int numberM = int.Parse(ReadLine());
+
+Write("Введите начальное число M: ");
+int numberN = int.Parse(ReadLine());
+
+GapNumberSum(numberM, numberN, 0);
+
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Write($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
